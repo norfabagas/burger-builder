971f7e6e4f640805dde57dbaaf7e8840978ca1fb
Norfa Bagas <bgsnorfa@gmail.com>
2020-12-01 02:28:56 +0700

    Setup higher-order-component and test it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render () {
     return (
       <div>
-
+        <Layout>
+          <p>Lorem Ipsum</p>
+        </Layout>
       </div>
-    )
+    );
   }
 }
 
